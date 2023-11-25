2c95a57976b5930319b331416b884d3e000a35a9
Quentin <vous@exemple.com>
2023-11-25 20:28:30 +0100

    création de la page listant les hackathons

--- hackatWeb/src/Controller/HackathonController.php
+++ hackatWeb/src/Controller/HackathonController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Hackathon;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HackathonController extends AbstractController
 {
     #[Route('/hackathon', name: 'app_hackathon')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('hackathon/index.html.twig', [
-            'controller_name' => 'HackathonController',
-        ]);
+        $repository = $doctrine->getRepository(Hackathon::class);
+        $lesHackathons = $repository->findBy([], ['dateDebut' => 'ASC']);
+
+        return $this->render('hackathon/index.html.twig', ['lesHackathons' => $lesHackathons]);
     }
 }
