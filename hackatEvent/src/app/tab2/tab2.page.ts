12e030731814e48c6bf23c004418633cbaa1f77b
Quentin <vous@exemple.com>
2023-12-22 13:41:35 +0100

    modifs page 2

--- hackatEvent/src/app/tab2/tab2.page.ts
+++ hackatEvent/src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { HackatServiceService } from '../hackat-service.service';
 
 @Component({
   selector: 'app-tab2',
@@ -8,12 +10,17 @@ import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 })
 export class Tab2Page {
   leHackathon:any;
+  lesEvenements:any;
   
-  constructor(private router: Router, private activeRoute: ActivatedRoute) {
+  constructor(private router: Router, public hackatService : HackatServiceService, private activeRoute: ActivatedRoute) {
     this.activeRoute.queryParams.subscribe(param=>{
       let navigation:any = this.router.getCurrentNavigation()?.extras.state;
       this.leHackathon = navigation.item;
     })
+
+    this.hackatService.getAteliers().then(results => { 
+      this.lesEvenements=results;
+    });
   }
 
 }
